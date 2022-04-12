fc1eb85242e67da80b6c557b7d6ac8fb9952b079
Mahmoud Arafat <mahmoudarafat33@gmail.com>
2022-04-12 14:34:24 +0200

    Add files via upload

---
+++ src/Controllers/EditHistoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MahmoudArafat\EditHistory\Controllers;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+
+class EditHistoryController extends Controller
+{
+    
+    public function test(){
+
+        return 'test';
+
+    }
+}
