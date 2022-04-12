5753d0d8a755e5d5eaaeaf8f8f805c2197137e6c
Mahmoud Arafat <mahmoudarafat33@gmail.com>
2022-04-12 14:32:20 +0200

    Add files via upload

---
+++ src/Models/EditHistory.php
@@ -0,0 +1,16 @@
+<?php
+namespace MahmoudArafat\EditHistory\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+* Model of the table tasks.
+*/
+class EditHistory extends Model
+{
+    protected $table = 'edit_histories';
+
+    protected $guarded = [];
+
+    
+}
