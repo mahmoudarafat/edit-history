a9d0d058fd967692a8320bd03534173a46170472
Mahmoud Arafat <mahmoudarafat33@gmail.com>
2022-04-13 08:44:59 +0200

    Update EditHistoryServiceProvider.php

--- src/EditHistoryServiceProvider.php
+++ src/EditHistoryServiceProvider.php
@@ -3,19 +3,29 @@
 namespace MahmoudArafat\EditHistory;
 
 use Illuminate\Support\ServiceProvider;
+use MahmoudArafat\EditHistory\Commands\tableCommand;
+use MahmoudArafat\EditHistory\Controllers\EditHistoryController;
 
 class EditHistoryServiceProvider extends ServiceProvider
 {
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
-        $this->loadMigrationsFrom(__DIR__.'/migrations');
-        $this->loadViewsFrom(__DIR__.'/views', 'history-views');
-        $this->publishes([
-            __DIR__.'/config/edit-history.php' => base_path('config/edit-history.php'),
-           // __DIR__.'/views' => base_path('resources/views/mahmoud-arafat/edit-history'),
-        ]);
+
+
+        include __DIR__.'/routes/routes.php';
+        include __DIR__.'/Classes/helpers.php';
+	
+		if(! file_exists(base_path('config/edit-history.php')))
+		{
+			$this->publishes([ __DIR__.'/config' => base_path('config')]);
+		}
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                tableCommand::class
+            ]);
+        }
     }
 
     /**
@@ -26,6 +36,13 @@ class EditHistoryServiceProvider extends ServiceProvider
 
     public function register()
     {
-        //$this->app->make('MahmoudArafat\EditHistory\Controllers\EditHistoryController');
+        $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+        $this->loadViewsFrom(__DIR__.'/views', 'history-views');
+
+        $this->app->alias(EditHistoryController::class, 'EditHistory');
+        
+        $this->app->make('mahmoudarafat\edit-history\Controllers\EditHistoryController');
+
     }
 }
