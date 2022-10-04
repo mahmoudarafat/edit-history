0de1da19aa8ee57655ceb43ae16bd96877e1571a
Mahmoud Arafat <mahmoudarafat33@gmail.com>
2022-10-04 14:26:09 +0200

    Update Historyable.php

--- src/Traits/Historyable.php
+++ src/Traits/Historyable.php
@@ -11,23 +11,55 @@ trait Historyable
 {
 
     public static function bootHistoryable(){
+        
         static::updated( function(Model $model){
+           /* $changes = $model->getChanges();
+
+          
+
+            $changes = $model->getWantedChangedColumns($model);
+            
+            foreach($changes as $change){
+           
+                $model->saveChange($change);
+            }
+            die();*/
+            
             collect($model->getWantedChangedColumns($model))->each(function ($change) use($model){
                 $model->saveChange($change);
                 //$model->update($model->getChanges());
             });
+            
         } );
     }
 
     public function saveChange(ColumnChange $change){
+        
+        dump('2422', $change);
+
         $this->history()->create([
             'column_changed' => $change->column,
-            'original_value' =>$change->from,
-            'new_value' => $change->to,
+            'original_value' =>$this->valueToString($change->from),
+            'new_value' => $this->valueToString($change->to),
+            'edited_by' => auth()->check() ? auth()->id() : 'guest'
         ]);
     }
 
     protected function getWantedChangedColumns(Model $model){
+
+        $changes = $model->getChanges();
+        $ignored = $this->getIgnoredColumns($model);
+        $toTrack = [];
+        $original = $model->getOriginal();
+
+        foreach($changes as $key => $val){
+            if(! in_array($key, $ignored) ){
+                $toTrack[] = new ColumnChange($key, $this->valueToString(Arr::get($original, $key)),  $this->valueToString($val) );
+            }
+        }
+        return $toTrack;
+
+        /*
         return 
             collect(
                 array_diff(
@@ -35,9 +67,10 @@ trait Historyable
                     $original = $model->getOriginal()
                 )
             )->map(function ($change, $column) use ($original) {
-                return new ColumnChange($column, Arr::get($original, $column), $change);
+                
+                return new ColumnChange($column, $this->valueToString(Arr::get($original, $column)),  $this->valueToString($change) );
             });
-    
+        */
     }
 
     public function history()
@@ -48,6 +81,17 @@ trait Historyable
     public function getIgnoredColumns($model)
     {
         return $model->ignoreHistoryColumns ?? ['updated_at'];
+    }
+    
+    public function valueToString($value){
+        $type = gettype($value);
+        if($type == 'boolean'){
+            $value = $value ? '1' : '0';
+        }
+        if( in_array($type, ['NULL', 'null'])){
+            $value = '';
+        }
+        return (string) $value;
     }
 
 }
